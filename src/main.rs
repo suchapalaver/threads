e954f2d989e676ff822aab26d4104899398293e5
Joseph Livesey <jlivesey@gmail.com>
2023-08-22 10:20:53 -0400

    feat: implement sending response to client requests

--- src/main.rs
+++ src/main.rs
@@ -14,12 +14,13 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
-
     let http_request: Vec<_> = buf_reader
         .lines()
         .map(|result| result.unwrap())
         .take_while(|line| !line.is_empty())
         .collect();
 
-    println!("Request: {:#?}", http_request);
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+
+    stream.write_all(response.as_bytes()).unwrap();
 }
