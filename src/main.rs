10f7fb54b4a3c26c98592ee8308ab95b96267186
Joseph Livesey <jlivesey@gmail.com>
2023-08-22 11:51:56 -0400

    feat: add basic ThreadPool implementation

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,16 @@ use std::{
     time::Duration,
 };
 
+use threads::ThreadPool;
+
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
+
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        thread::spawn(|| {
+        pool.execute(|| {
             handle_connection(stream);
         });
     }
